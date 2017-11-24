d8691b2095a84a8e0c2c0fd329bf9fcc4aa3f1a3
SimpsonTuga <33097911+SimpsonTuga@users.noreply.github.com>
2017-11-24 13:23:24 +0000

    Views v0.3
    Mudificas views dos guias e partilhar

---
+++ Trails4Health/Migrations/20171124121126_Migracao2.Designer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Trails4Health.Data;
+
+namespace Trails4Health.Migrations
+{
+    [DbContext(typeof(Trails4HealthDbContext))]
+    [Migration("20171124121126_Migracao2")]
+    partial class Migracao2
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.4")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Trails4Health.Models.Guide", b =>
+                {
+                    b.Property<int>("IdGuide")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Email")
+                        .IsRequired();
+
+                    b.Property<string>("Name")
+                        .IsRequired();
+
+                    b.Property<string>("Phone")
+                        .IsRequired();
+
+                    b.HasKey("IdGuide");
+
+                    b.ToTable("Guide");
+                });
+        }
+    }
+}
--- Trails4Health/Models/Guide.cs
+++ Trails4Health/Models/Guide.cs
@@ -13,8 +13,7 @@ namespace Trails4Health.Models
         
         public string Name { get; set; }
         [Required(ErrorMessage = "Porfavor Introduza o numero de Telemovel")]
-        //[MaxLength="9"]
-        //[MinLength=9]
+        [RegularExpression(@"(2\d{8})|(9[1236]\d{7})", ErrorMessage = "Telemóvel inválido")]
         public string Phone { get; set; }
         [Required(ErrorMessage = "Porfavor Introduza o Email")]
         [RegularExpression(".+\\@.+\\..+",ErrorMessage ="Porfavor introduza um email valido")]
